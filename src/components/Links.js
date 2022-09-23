94ac29ccffcd978737b12e672c13e8d251d73b79
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-23 11:13:09 +0300

    submitting changes

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links(props){
+    return(
+        <div>
+            <h3>Links</h3>
+                <a href ='https://github.com/liza'> {props.git} </a>
+                <a href ='https://www.linkedin.com/in/liza'>  {props.linkedin} </a>
+            
+        </div>
+    )
+}
+
+export default Links;
